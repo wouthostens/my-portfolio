1dea6f52a7b9319563c4d98f77f68fb3e32e991b
Wout Hostens <Wout.hostens@student.vives.be>
2024-04-17 09:15:33 +0200

    Added stravaiframe component for read ability

---
+++ src/components/stravaiframe.vue
@@ -0,0 +1,20 @@
+<template>
+    <iframe class="w-full sm:w-3/4 lg:w-1/2 dark:bg-slate-900 bg-white mb-2" :height="height" frameborder='0'
+      allowtransparency='true' scrolling='no'
+      :src="'https://www.strava.com/athletes/7422402/' + src + '/fa96379fe34efd1a6d0a7982ff862e0c6f5a284b'"></iframe>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      src: {
+        type: String,
+        required: true
+      },
+      height: {
+        type: String,
+        default: '160'
+      }
+    }
+  }
+  </script>
--- src/pages/strava.vue
+++ src/pages/strava.vue
@@ -2,6 +2,7 @@
 import TitleComponent from '../components/title.vue';
 import BoxComponent from '../components/box.vue';
 import DescriptionComponent from '../components/description.vue';
+import StravaIframe from '../components/stravaiframe.vue';
 </script>
 
 <template>
@@ -16,16 +17,12 @@ import DescriptionComponent from '../components/description.vue';
     <div class="h-85vh overflow-auto ">
       <BoxComponent extraClass=" flex flex-col items-center mx-auto sm:w-3/4 lg:w-1/2 w-full">
         <DescriptionComponent>
-          Hier kan je een oplijsting zien van mijn laatste loopjes alsook een overzicht van deze week. Ik loop
+          Hier kan je  een overzicht van deze week zien alsook een oplijsting van mijn laatste 5 loopjes. Ik loop
           graag als ontspanning, maar alleen wanneer het weer aangenaam is. Dit doe ik meestal in de omgeving van
           Avelgem. Het doel is om ooit een marathon te lopen.
         </DescriptionComponent>
-        <iframe class="mb-3 w-full sm:w-3/4 lg:w-1/2 dark:bg-slate-900 bg-white" height='454' frameborder='0'
-          allowtransparency='true' scrolling='no'
-          src='https://www.strava.com/athletes/7422402/latest-rides/fa96379fe34efd1a6d0a7982ff862e0c6f5a284b'></iframe>
-        <iframe class="w-full sm:w-3/4 lg:w-1/2 dark:bg-slate-900 bg-white" height='160 ' frameborder='0'
-          allowtransparency='true' scrolling='no'
-          src='https://www.strava.com/athletes/7422402/activity-summary/fa96379fe34efd1a6d0a7982ff862e0c6f5a284b'></iframe>
+        <StravaIframe src="activity-summary" />
+        <StravaIframe src="latest-rides" height="454" />
       </BoxComponent>
     </div>
   </div>

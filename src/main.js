0e0892612f68e96b4396708da04d4c77502ef0c6
Wout Hostens <Wout.hostens@student.vives.be>
2024-04-17 08:58:52 +0200

    back to the beginning

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import VueMeta from 'vue-meta'
 
-const app = createApp(App)
-app.use(VueMeta)
-app.mount('#projects')
+
+createApp(App).mount('#projects')
